6c48d89914f63cfd6cd47f3f240ba016c9c9c678
Abderahmane Bouziane <bobmatt911@gmail.com>
2026-01-10 12:49:54 -0500

    Vendor lazygit and ueberzugpp configs

--- modules/home-manager/vendor-configs.nix
+++ modules/home-manager/vendor-configs.nix
@@ -31,6 +31,10 @@ _: {
     "gh-dash/config.yml".source = ../../config/gh-dash/config.yml;
 
     "marimo/marimo.toml".source = ../../config/marimo/marimo.toml;
+
+    "lazygit/config.yml".source = ../../config/lazygit/config.yml;
+
+    "ueberzugpp/config.json".source = ../../config/ueberzugpp/config.json;
   };
 
   # Home-root dotfiles
